12309c1114eec580300d36e26d01a3063a8f757a
jhonerick31 <jhonerickrecaro41@gmail.com>
2025-09-18 13:57:17 +0800

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -59,15 +59,27 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'sql.freedb.tech',
+    'hostname'	=> getenv("DB_HOST"),
+    'port'		=> getenv('DB_PORT'),
+    'username'	=> getenv("DB_USER"),
+    'password'	=> getenv("DB_PASS"),
+    'database'	=> getenv("DB_NAME"),
+    'charset'	=> 'utf8mb4',
+    'dbprefix'	=> '',
+);
+
+$database['main1'] = array(
+    'driver'	=> 'mysql',
+    'hostname'	=> 'localhost',
     'port'		=> '3306',
-    'username'	=> 'freedb_erick',
-    'password'	=> '8P%7V*qd3*3k$JW',
-    'database'	=> 'freedb_studentsdb',
+    'username'	=> 'root',
+    'password'	=> '',
+    'database'	=> 'api',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
     'path'      => ''
 );
+
 
 ?>
