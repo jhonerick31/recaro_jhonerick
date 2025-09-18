13be2360e992589413318ca7ffd785b86795e977
jhonerick31 <jhonerickrecaro41@gmail.com>
2025-09-18 16:08:18 +0800

    update

--- app/config/database.php
+++ app/config/database.php
@@ -57,24 +57,13 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 |   Example: $database['another_example'] = array('key' => 'value')
 */
 
-$database['main'] = array(
-    'driver'	=> 'mysql',
-    'hostname'	=> getenv("DB_HOST"),
-    'port'		=> getenv('DB_PORT'),
-    'username'	=> getenv("DB_USER"),
-    'password'	=> getenv("DB_PASS"),
-    'database'	=> getenv("DB_NAME"),
-    'charset'	=> 'utf8mb4',
-    'dbprefix'	=> '',
-);
-
-$database['main1'] = array(
+  $database = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'mockdata',
+    'username'	=> 'sql12799103',
+    'password'	=> 'nZg2GADW3e',
+    'database'	=> 'sql12799103',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
